64ea57255f8227debd338387a405db7e74ae4521
lxy-yz <lxy.dev.yz@gmail.com>
2022-03-07 21:30:39 -0800

    filesystem routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,21 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useRoutes, BrowserRouter as Router } from "react-router-dom";
+
+import routes from "~react-pages";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Router>
+        <Routes />
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+function Routes() {
+  return useRoutes(routes);
+}
+
+export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Index: React.FC = () => {
+  return (
+    <div>
+      <Link to="/blog">blog</Link> |<Link to="/xxx">404</Link>
+    </div>
+  );
+};
+
+export default Index;
