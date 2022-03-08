5dbe6d7cbbe0965dd0c532ab49cb821e5e32737a
lxy-yz <lxy.dev.yz@gmail.com>
2022-03-07 22:08:36 -0800

    mock server

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const prepare = async () => {
+  if (import.meta.env.DEV) {
+    const { default: worker } = await import("./mocks/browser");
+    return worker.start();
+  }
+};
+
+prepare().then(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+});
